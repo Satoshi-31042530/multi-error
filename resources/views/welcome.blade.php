5686a48b97299ea6ea2f7891f8fc0e74054b0111
Satoshi-31042530 <i.am.satoshi2530@gmail.com>
2021-06-07 21:40:56 +0900

    タイトル変更

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Multi-login
                 </div>
 
                 <div class="links">
